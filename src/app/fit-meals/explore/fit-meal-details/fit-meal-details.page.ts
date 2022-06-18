19de41b695a19ac70bc926fcf5638a24bfe5213f
Milica Djokic <milica.djjokic@gmail.com>
2022-06-18 15:59:14 +0200

    sredjivanje details page

--- src/app/fit-meals/explore/fit-meal-details/fit-meal-details.page.ts
+++ src/app/fit-meals/explore/fit-meal-details/fit-meal-details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FitMeal } from '../../fit-meal.model';
 
 @Component({
   selector: 'app-fit-meal-details',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fit-meal-details.page.scss'],
 })
 export class FitMealDetailsPage implements OnInit {
+
+ fitmeal: FitMeal = {id: 'r1', title: 'Spicy Chicken With Couscous', text: 'This super-tasty macro-balanced meal is perfect for nailing that meal prep. Bursting with flavour, it’s a simple way to liven up your routine from plain old chicken and rice.',protein:'50g',imageUrl: 'https://blogscdn.thehut.net/app/uploads/sites/478/2019/12/Spicy-Chicken-ARTICLE_1577793747.jpg'};
 
   constructor() { }
 
